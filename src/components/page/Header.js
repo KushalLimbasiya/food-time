70797c043cc4ba78270a0693bc3822fa4f1f6fe6
Kushal <kushallimbasiya@gmail.com>
2023-04-23 13:01:58 +0530

    add header and css

---
+++ src/components/page/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/main.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+
+function Header () {
+    return (
+        <>
+            <div>
+                <header>
+                    <a href="#home" className="logo">
+                    <FontAwesomeIcon icon={faUtensils}  color="red" /> food</a>
+            
+                    <div id="menu-bar" className="fas fa-bars"> </div>
+            
+                    <nav className="navbar">
+                        <a href="#home">home</a>
+                        <a href="#Menu">Menu</a>
+                        <a href="#popular">Popular</a>
+                        <a href="#gallery">Gallery</a>
+                        <a href="#review">Review</a>
+                        <a href="#order">Order</a>
+                    </nav>
+                </header>
+            </div>
+        </>
+    )
+};
+
+export default Header;
