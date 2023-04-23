cb8f4c49e59050b01e5b676b70f89bbe691e79c5
Kushal <kushallimbasiya@gmail.com>
2023-04-23 12:54:53 +0530

    added images and home page

--- src/compnents/page/Home.js
+++ src/compnents/page/Home.js
@@ -7,7 +7,7 @@ function Home() {
         <>
             <section className="home" id="home">
                 <div className="content">
-                    <h3> food made with love   </h3>
+                    <h3> food made with love  </h3>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores deserunt voluptate adipisci impedit, deleniti ipsa culpa explicabo ea, temporibus fugiat. Accusamus debitis adipisci saepe perferendis consequuntur laboriosam ab nam veniam quis qui aliquam vitae, animi dolorum iste repellat eveniet dolore ex doloremque velit incidunt assumenda quam error minima Vitae.
                     </p>
