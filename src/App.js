e59a2a5b1c3bb8130fd11d74c63d28208684e58e
Kushal <kushallimbasiya@gmail.com>
2023-04-23 13:10:49 +0530

    home page complate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Home from './components/page/main';
+import Main from './components/page/Main';
 
 const App = () => {
     return (
         <div>
-            <Home />
+            <Main />
         </div>
     )
 };
---
+++ src/components/page/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+
+
+function Main() {
+    return (
+        <>
+            <Header />
+            <Home />
+        </>
+    )
+};
+
+export default Main;
