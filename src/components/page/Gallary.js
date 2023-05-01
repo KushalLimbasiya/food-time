8adb6ebabb0c979dee6d35b09262de07a3720bc7
Kushal <kushallimbasiya@gmail.com>
2023-05-01 21:05:40 +0530

    add proper image path and css

--- src/components/page/Gallary.js
+++ src/components/page/Gallary.js
@@ -1,84 +1,84 @@
 import React from 'react';
+import * as ig from '../images/img.js';
 
 const Gallary = () => {
     return (
-        <section div='gallery' id='gallery'>
+        <section className='gallery' id='gallery'>
             <h1 class="heading"> our food <span>gallery</span></h1>
         
-            <div class="box-container">
-                
-                <div class="box">
-                    <img src="images/g-1.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
+                <div class="box-container">
+                    
+                    <div class="box">
+                        <img src={ig.g1} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src={ig.g2} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div><div class="box">
+                        <img src={ig.g3} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src={ig.g4} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div><div class="box">
+                        <img src={ig.g5} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src={ig.g6} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src={ig.g7} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src={ig.g8} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src={ig.g9} alt=""/>
+                        <div class="content">
+                            <h3>testy food</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
+                            <a href="#" class="btn">order now</a>
+                        </div>
                     </div>
                 </div>
-                <div class="box">
-                    <img src="images/g-2.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div><div class="box">
-                    <img src="images/g-4.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="images/g-3.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div><div class="box">
-                    <img src="images/g-9.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="images/g-6.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="images/g-7.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="images/g-8.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="images/g-5.jpg" alt=""/>
-                    <div class="content">
-                        <h3>testy food</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, saepe!</p>
-                        <a href="#" class="btn">order now</a>
-                    </div>
-                </div>
-            </div>
-
         </section>
     )
 }
