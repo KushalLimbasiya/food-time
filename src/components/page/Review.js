efcce4e8959bd5ae438f35cb05f6cfc6b85108bf
Kushal <kushallimbasiya@gmail.com>
2023-05-01 21:16:02 +0530

    add description on review

--- src/components/page/Review.js
+++ src/components/page/Review.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as ig from '../images/img.js';
 
 const Review = () => {
     return (
@@ -6,8 +7,19 @@ const Review = () => {
              <h1 class="heading"> our customers <span>reviews</span></h1>
              <div className='box-container'>
                 <div className='box'>
-                    <img src={'#'} alt='/' />
+                    <img src={ig.pic1} alt='/' />
                     <h3>bruce lee</h3>
+                    <p>Absolutely delicious! The flavors were perfectly balanced and the presentation was beautiful. Can't wait to order from here again!</p>
+                </div>
+                <div className='box'>
+                    <img src={ig.pic2} alt='/' />
+                    <h3>bruce lee</h3>
+                    <p>The food arrived hot and fresh, and the portions were very generous. The prices are reasonable and the taste is amazing!</p>
+                </div>
+                <div className='box'>
+                    <img src={ig.pic3} alt='/' />
+                    <h3>bruce lee</h3>
+                    <p>This food was a game-changer! I had never tasted such amazing flavors before. The customer service was also excellent. Highly recommended!</p>
                 </div>
              </div>
         </section>
