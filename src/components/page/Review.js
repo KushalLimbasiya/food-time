976f7f7204485ced191fca9e3f93b6ff8de44e07
Kushal <kushallimbasiya@gmail.com>
2023-05-01 21:22:20 +0530

    add start rating in review using fontawesome

--- src/components/page/Review.js
+++ src/components/page/Review.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ig from '../images/img.js';
+import { Start } from './StarRating.js';
 
 const Review = () => {
     return (
@@ -9,16 +10,19 @@ const Review = () => {
                 <div className='box'>
                     <img src={ig.pic1} alt='/' />
                     <h3>bruce lee</h3>
+                    <Start />
                     <p>Absolutely delicious! The flavors were perfectly balanced and the presentation was beautiful. Can't wait to order from here again!</p>
                 </div>
                 <div className='box'>
                     <img src={ig.pic2} alt='/' />
                     <h3>bruce lee</h3>
+                    <Start />
                     <p>The food arrived hot and fresh, and the portions were very generous. The prices are reasonable and the taste is amazing!</p>
                 </div>
                 <div className='box'>
                     <img src={ig.pic3} alt='/' />
                     <h3>bruce lee</h3>
+                    <Start />
                     <p>This food was a game-changer! I had never tasted such amazing flavors before. The customer service was also excellent. Highly recommended!</p>
                 </div>
              </div>
